03b6f6d0aaee5c0437b65bcb85ed1e92874e1b81
alfarramazanputra <alfarbogor@gmail.com>
2024-06-10 12:01:25 +0700

    update route user

--- src/routes/users.js
+++ src/routes/users.js
@@ -7,14 +7,10 @@ const upload = require('../config/multerConfig')
 route.get('/', UserController.getAllUsers)
 route.get('/:username', UserController.getUsersbyUsername)
 
-//CREATE
-// route.post('/', upload.single('image'), UserController.createNewUsers)
-
 //UPDATE
 route.patch('/:id', upload.single('image'), UserController.updateUsers)
 
 //DELETED
 route.delete('/:id', UserController.deleteUsers) 
-    
 
 module.exports = route;
